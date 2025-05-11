4198a16a2b587fe9c9edf9a925afcfad7a945f6d
tinhtt <tinhtrantrung.vn@gmail.com>
2025-05-11 13:31:56 +1200

    Update text sizes.

--- src/app.jsx
+++ src/app.jsx
@@ -14,22 +14,22 @@ const App = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <main className="container-auto">
-        <header className="flex h-16 w-full justify-end gap-4">
+        <header className="flex h-16 w-full items-center justify-between gap-4">
           <RandomQuestion items={questions} />
           <ModeToggle />
         </header>
 
         <section className="flex-center flex-col">
-          <h1 className="py-12 text-5xl font-bold">
+          <h1 className="py-12 text-3xl font-bold sm:text-4xl md:text-5xl">
             Interview Questions and Answers
           </h1>
           <Accordion type="single" collapsible className="w-full max-w-3xl">
             {questions.map((item, index) => (
               <AccordionItem key={index} value={`item-${index}`}>
-                <AccordionTrigger className="cursor-pointer text-2xl">
+                <AccordionTrigger className="min-h-20 cursor-pointer text-lg sm:text-xl md:text-2xl">
                   {`${index + 1}. ${item.question}`}
                 </AccordionTrigger>
-                <AccordionContent className="text-lg">
+                <AccordionContent className="text-sm sm:text-lg">
                   {item.answer}
                 </AccordionContent>
               </AccordionItem>
