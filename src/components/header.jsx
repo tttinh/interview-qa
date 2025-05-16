b2249c9631b7081218ea2840a4ff2116aec24724
tinhtt <tinhtrantrung.vn@gmail.com>
2025-05-16 14:38:51 +1200

    Implement dropdown.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import { ModeToggle } from './mode-toggle';
+import { ModeToggle } from '@/components/mode-toggle';
 import { NavLink } from 'react-router';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Button } from './ui/button';
+import { Menu } from 'lucide-react';
 
 const links = [
   {
@@ -17,9 +24,8 @@ const links = [
   },
 ];
 
-const activeStyle = 'text-primary px-2 py-2 rounded-md';
-
-const normalStyle = 'text-primary/65 hover:text-primary px-2 py-2 rounded-md';
+const activeStyle = 'text-primary font-semibold px-2 py-2';
+const normalStyle = 'text-primary/65 hover:text-primary px-2 py-2';
 
 const Header = () => {
   return (
@@ -27,7 +33,7 @@ const Header = () => {
       <h2 className="font-bold">Enjoy your journey!</h2>
 
       <div className="flex items-center gap-4">
-        <nav className="flex gap-1">
+        <nav className="hidden gap-1 lg:flex">
           {links.map((link, index) => (
             <NavLink
               key={index}
@@ -41,6 +47,31 @@ const Header = () => {
             </NavLink>
           ))}
         </nav>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger className="lg:hidden" asChild>
+            <Button variant="outline" size="icon">
+              <Menu />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            {links.map((link, index) => (
+              <DropdownMenuItem>
+                <NavLink
+                  key={index}
+                  to={link.href}
+                  end
+                  className={({ isActive }) =>
+                    `w-full ${isActive ? activeStyle : normalStyle}`
+                  }
+                >
+                  {link.label}
+                </NavLink>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+
         <ModeToggle />
       </div>
     </header>
